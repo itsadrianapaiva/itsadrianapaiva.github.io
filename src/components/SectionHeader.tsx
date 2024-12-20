0353ce613f32fb7adf38a5dc9448af6d7be48699
unknown <adrianapaivausa@icloud.com>
2024-12-20 17:19:54 -0500

    component for section header created

---
+++ src/components/SectionHeader.tsx
@@ -0,0 +1,29 @@
+
+
+const SectionHeader = ({
+    title,
+    eyebrow,
+    description,
+}: {
+    title: string;
+    eyebrow: string;
+    description: string;
+}) => {
+  return (
+    <>
+    <div className='flex justify-center'>
+          <p className='to-intense-pink bg-gradient-to-r from-medium-pink bg-clip-text text-center font-semibold uppercase tracking-widest text-transparent'>
+            {eyebrow}
+          </p>
+        </div>
+        <h2 className='mt-6 text-center font-serif text-3xl m d:text-5xl'>
+          {title}
+        </h2>
+        <p className='mx-auto mt-4 max-w-md text-center text-white/60 md:text-lg lg:text-xl'>
+          {description}
+        </p>
+    </>
+  )
+}
+
+export default SectionHeader
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import CheckCircleIcon from '@/assets/icons/check-circle.svg';
 import ArrowUpRightIcon from '@/assets/icons/arrow-up-right.svg';
 import grainImage from '@/assets/images/grain.jpg';
+import SectionHeader from '@/components/SectionHeader';
 
 const portfolioProjects = [
   {
@@ -67,18 +68,7 @@ export const ProjectsSection = () => {
   return (
     <section className='pb-16 lg:py-24'>
       <div className='container'>
-        <div className='flex justify-center'>
-          <p className='to-intense-pink bg-gradient-to-r from-medium-pink bg-clip-text text-center font-semibold uppercase tracking-widest text-transparent'>
-            Real-world Results
-          </p>
-        </div>
-        <h2 className='mt-6 text-center font-serif text-3xl md:text-5xl'>
-          Featured Projects
-        </h2>
-        <p className='mx-auto mt-4 max-w-md text-center text-white/60 md:text-lg lg:text-xl'>
-          See how I transformed concepts into modern and engaging digital
-          experiences.
-        </p>
+        <SectionHeader eyebrow="Real-world Results" title="Featured Projects" description='See how I transformed concepts into modern and engaging digital experiences.'/>
 
         <div className='mt-10 flex flex-col gap-20 md:mt-20'>
           {portfolioProjects.map((project) => (
