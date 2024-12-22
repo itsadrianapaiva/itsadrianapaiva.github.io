d79f57891bd86b9c6ae4e02758fd990546b0817d
unknown <adrianapaivausa@icloud.com>
2024-12-22 18:42:45 -0500

    about me section in progress

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,3 +1,74 @@
+import Card from '@/components/Card';
+import SectionHeader from '@/components/SectionHeader';
+import StarIcon from '@/assets/icons/star.svg';
+import Image from 'next/image';
+import bookCover from '@/assets/images/book.jpg';
+
+const toolboxItems = [
+  {
+    title: 'JavaScript',
+    icon: '',
+  },
+  {
+    title: 'TypeScript',
+    icon: '',
+  },
+  {
+    title: 'Tailwind CSS',
+    icon: '',
+  },
+  {
+    title: 'Git',
+    icon: '',
+  },
+  {
+    title: 'Node.js',
+    icon: '',
+  },
+  {
+    title: 'React',
+    icon: '',
+  },
+  {
+    title: 'Next.js',
+    icon: '',
+  },
+  {
+    title: 'Python',
+    icon: '',
+  },
+
+]
+
 export const AboutSection = () => {
-  return <div>About Section</div>;
+  return (
+    <div className='pb-96'>
+      <SectionHeader
+        eyebrow='About Me'
+        title='Glimpse Into My World'
+        description='Learn more about who I am, what I do, and what inspires me.'
+      />
+
+      <div>
+        <Card>
+          <div>
+          <StarIcon />
+          <h3>My Reads</h3>
+          <p>Explore the books shaping my perspectives.</p>
+          </div>
+          <Image src={bookCover} alt='Book Cover' />
+        </Card>
+        <Card>
+          <div>
+          <StarIcon />
+          <h3>My Toolbox</h3>
+          <p>Explore the technologies and tools that I use to craft exceptional digital experiences.</p>
+          </div>
+          <div>
+
+          </div>
+        </Card>
+      </div>
+    </div>
+  );
 };
