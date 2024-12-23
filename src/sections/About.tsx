e5f0ad386c393bdb08da97c986c593623698ee93
unknown <adrianapaivausa@icloud.com>
2024-12-22 19:37:32 -0500

    bento box in progress

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -3,71 +3,176 @@ import SectionHeader from '@/components/SectionHeader';
 import StarIcon from '@/assets/icons/star.svg';
 import Image from 'next/image';
 import bookCover from '@/assets/images/book.jpg';
+import JavaScriptIcon from '@/assets/icons/square-js.svg';
+import ReactIcon from '@/assets/icons/react.svg';
+import GitHubIcon from '@/assets/icons/github.svg';
+import PythonIcon from '@/assets/icons/python.svg';
+import MongoDBIcon from '@/assets/icons/mongodb.svg';
+import SQLIcon from '@/assets/icons/sql.svg';
+import NextIcon from '@/assets/icons/nextjs.svg';
+import TailwindIcon from '@/assets/icons/tailwindcss.svg';
+import NodeIcon from '@/assets/icons/nodejs.svg';
+import GitIcon from '@/assets/icons/gitIcon.svg';
+import TypeScriptIcon from '@/assets/icons/typescriptIcon.svg';
+import CloudIcon from '@/assets/icons/googlecloud.svg';
+import TechIcon from '@/components/TechIcon';
+import mapImage from '@/assets/images/map.png';
+import myMemoji from '@/assets/images/adriana-avatar-laptop.png';
 
 const toolboxItems = [
   {
     title: 'JavaScript',
-    icon: '',
+    iconType: JavaScriptIcon,
   },
   {
     title: 'TypeScript',
-    icon: '',
+    iconType: TypeScriptIcon,
   },
   {
     title: 'Tailwind CSS',
-    icon: '',
+    iconType: TailwindIcon,
   },
   {
     title: 'Git',
-    icon: '',
+    iconType: GitIcon,
+  },
+  {
+    title: 'GitHub',
+    iconType: GitHubIcon,
   },
   {
     title: 'Node.js',
-    icon: '',
+    iconType: NodeIcon,
   },
   {
     title: 'React',
-    icon: '',
+    iconType: ReactIcon,
   },
   {
     title: 'Next.js',
-    icon: '',
+    iconType: NextIcon,
   },
   {
     title: 'Python',
-    icon: '',
+    iconType: PythonIcon,
   },
+  {
+    title: 'SQL',
+    iconType: SQLIcon,
+  },
+  {
+    title: 'MongoDB',
+    iconType: MongoDBIcon,
+  },
+  {
+    title: 'Cloud Services',
+    iconType: CloudIcon,
+  },
+];
 
-]
+const hobbies = [
+  {
+    title: 'Running',
+    emoji: '🏃‍♀️',
+  },
+  {
+    title: 'Family Time',
+    emoji: '👨‍👩‍👦',
+  },
+  {
+    title: 'Fashion',
+    emoji: '👗 ',
+  },
+  {
+    title: 'Nature',
+    emoji: '🍃',
+  },
+  {
+    title: 'Photography',
+    emoji: '📸',
+  },
+  {
+    title: 'Journaling',
+    emoji: '📓',
+  },
+  {
+    title: 'Traveling',
+    emoji: '✈',
+  },
+];
 
 export const AboutSection = () => {
   return (
-    <div className='pb-96'>
-      <SectionHeader
-        eyebrow='About Me'
-        title='Glimpse Into My World'
-        description='Learn more about who I am, what I do, and what inspires me.'
-      />
+    <div className='py-20'>
+      <div className='container'>
+        <SectionHeader
+          eyebrow='About Me'
+          title='Glimpse Into My World'
+          description='Learn more about who I am, what I do, and what inspires me.'
+        />
 
-      <div>
-        <Card>
-          <div>
-          <StarIcon />
-          <h3>My Reads</h3>
-          <p>Explore the books shaping my perspectives.</p>
-          </div>
-          <Image src={bookCover} alt='Book Cover' />
-        </Card>
-        <Card>
-          <div>
-          <StarIcon />
-          <h3>My Toolbox</h3>
-          <p>Explore the technologies and tools that I use to craft exceptional digital experiences.</p>
-          </div>
-          <div>
-
-          </div>
-        </Card>
+        <div className='mt-20'>
+          <Card className="h-[320px]">
+            <div className='flex flex-col'>
+              <div className='inline-flex items-center gap-2'>
+                <StarIcon className='size-9 text-medium-pink' />
+                <h3 className='font-serif text-3xl'>My Reads</h3>
+              </div>
+              <p className='mt-2 text-sm text-white/60'>
+                Explore the books shaping my perspectives.
+              </p>
+            </div>
+            <div className='mx-auto mt-8 w-40'>
+              <Image
+                src={bookCover}
+                alt='Book Cover'
+              />
+            </div>
+          </Card>
+          <Card>
+            <div>
+              <StarIcon />
+              <h3>My Toolbox</h3>
+              <p>
+                Explore the technologies and tools that I use to craft
+                exceptional digital experiences.
+              </p>
+            </div>
+            <div>
+              {toolboxItems.map((item) => (
+                <div key={item.title}>
+                  <TechIcon component={item.iconType} />
+                  <span>{item.title}</span>
+                </div>
+              ))}
+            </div>
+          </Card>
+          <Card>
+            <div>
+              <StarIcon />
+              <h3>Beyond the Code</h3>
+              <p>Explore my interests and hobbies beyond the digital realm.</p>
+            </div>
+            <div>
+              {hobbies.map((hobby) => (
+                <div key={hobby.title}>
+                  <span>{hobby.title}</span>
+                  <span>{hobby.emoji}</span>
+                </div>
+              ))}
+            </div>
+          </Card>
+          <Card>
+            <Image
+              src={mapImage}
+              alt='Map'
+            />
+            <Image
+              src={myMemoji}
+              alt='Memoji'
+            />
+          </Card>
+        </div>
       </div>
     </div>
   );
